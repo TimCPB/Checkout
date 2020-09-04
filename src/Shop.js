f6fc9d9bd77b5977f8f270bf82f19a0e3722186d
Tim <TimCPB@github.com>
2020-09-04 17:02:46 +0100

    _calculateTotal private function

--- src/Shop.js
+++ src/Shop.js
@@ -6,20 +6,7 @@ class Shop {
 
   checkout(items){
     let itemArray = items.split('')
-    // if(itemArray.includes(!'A', !'B', !'C', !'D')){
-    //   this.basketTotal = -1 }
-    //   else {
-    // itemArray.forEach(function(item) {
-    //     if(item === 'A'){ 
-    //       this.basketTotal += 50
-    //     } else if(item === 'B'){
-    //       this.basketTotal += 30
-    //     } else if(item === 'C'){
-    //       this.basketTotal += 20
-    //     } else if(item === 'D') {
-    //       this.basketTotal += 15 
-    //     } else { this.containsIllegalItem = true }
-    //   }, this)
+
     this._calculateTotal(itemArray)
 
     if(this.containsIllegalItem === false) {
